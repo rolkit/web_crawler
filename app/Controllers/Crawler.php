e22620d5f70aaae6b4559a9b339484bed67d4fa6
yusufmalikul <yusuflinuxer@gmail.com>
2024-04-06 14:30:56 +0700

    crawler basic

--- app/Controllers/Crawler.php
+++ app/Controllers/Crawler.php
@@ -3,12 +3,37 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use DOMDocument;
+use DOMXPath;
 
 class Crawler extends BaseController
 {
     public function index()
     {
-        // insert crawler logic here
+        // IMDb movie URL
+        $url = 'https://www.imdb.com/title/tt15398776/';
+
+        // Get the HTML content of the page
+        $html = file_get_contents($url);
+
+        // Create a DOMDocument object
+        $dom = new DOMDocument();
+
+        // Load the HTML content into the DOMDocument
+        @$dom->loadHTML($html);
+
+        // Create a DOMXPath object
+        $xpath = new DOMXPath($dom);
+
+        // Parse the title
+        $title = $xpath->query('//title')->item(0)->nodeValue;
+
+        // Parse the cover image
+        $coverImage = $xpath->query('//meta[@property="og:image"]')->item(0)->getAttribute('content');
+
+        // Output the results
+        echo '<h1>' . $title . '</h1>';
+        echo '<img width="20%" src="' . $coverImage . '"></img><br><br>';
+        echo 'Source: ' . $url;
     }
 }
