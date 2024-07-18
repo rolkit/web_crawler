8218cb941b17ae322bafedc0d367617b8cc614d4
rolkit <febrians_94@yahoo.com>
2024-07-18 22:31:59 +0700

    stop dellay

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/crawler', 'Crawler::index');
+$routes->get('/crawler', 'Craw::index');
 // $routes->get('/craw', 'Craw::index');
 // $routes->get('/crawler', 'Crawler::index');
